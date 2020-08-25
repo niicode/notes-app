7b77216d864828807f75bfd5ffc663b24c14475f
Niidarku <niidarku66@gmail.com>
2020-08-25 10:43:57 +0000

    creating a notes-app

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ yargs.command({
             type: 'string'
         },
         body:{
-            describe: 'Add text here',
+            describe: 'This is the not body'
             demandOption: true,
             type: 'string'
         }
